0792042a3a35d337e3b605ab5298f19a8badeac0
harry <hhallows96@gmail.com>
2021-01-29 18:43:48 +0000

    setting up spawnmanager

---
+++ GGJ2020/Assets/Scripts/Harrys/Projectiles/ProjectileTemp.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileTemp : MonoBehaviour, IPooledObjects
+{
+    // Start is called before the first frame update
+
+
+    public void OnObjectSpawn()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(0, 1, 0);
+
+        if (this.transform.position.y > 10)
+        {
+            this.gameObject.SetActive(false);
+        }
+    }
+}
