00682b7e62fdecb21b4c6ee8746d3fb3b85fa071
Harry Hallows <hhallows96@gmail.com>
2021-01-30 00:23:15 +0000

    spawnmanager attempt

--- GGJ2020/Assets/Scripts/Harrys/Projectiles/ProjectileTemp.cs
+++ GGJ2020/Assets/Scripts/Harrys/Projectiles/ProjectileTemp.cs
@@ -2,24 +2,56 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ProjectileTemp : MonoBehaviour, IPooledObjects
+public class ProjectileTemp : MonoBehaviour
 {
+
+    [SerializeField] private Transform[] targetObjects;
+    [SerializeField] private GameObject player;
+
+    [SerializeField] private Rigidbody2D rb;
+
+
+    [SerializeField] private float moveSpeed;
+
+
+    [SerializeField] private Transform target;
+    [SerializeField] private float speed;
+    [SerializeField] private Vector3 offset;
+
     // Start is called before the first frame update
+    
 
-
-    public void OnObjectSpawn()
+    private void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position += new Vector3(0, 1, 0);
+        transform.Translate(transform.right * moveSpeed * Time.deltaTime, Space.World);
+        
 
         if (this.transform.position.y > 10)
+        {
+            Destroy(gameObject,3f);
+        }
+
+        if (this.transform.position.y < -10)
+        {
+            this.gameObject.SetActive(false);
+        }
+
+        if (this.transform.position.x > 20)
+        {
+            Destroy(gameObject, 3f);
+        }
+
+        if (this.transform.position.x < -20)
         {
             this.gameObject.SetActive(false);
         }
     }
+
+
 }
