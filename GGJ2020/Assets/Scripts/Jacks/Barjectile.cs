87c1b6e3642e11af2ee920e91092c28c3de605dc
Jack Russell Brindle <jackrussellbrindle@outlook.com>
2021-01-31 12:21:19 +0000

    Bar

---
+++ GGJ2020/Assets/Scripts/Jacks/Barjectile.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Barjectile : MonoBehaviour
+{
+    float frametime = 24;
+    float delaygrowthtime ;
+
+    float despawnSize;
+    
+
+    float destoryDelay = 3f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        delaygrowthtime = Random.Range(20f, 40f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scaleIncrease();
+    }
+    
+    void scaleIncrease()
+    {
+        frametime -= 1;
+        if (frametime <= 0)
+        {
+            frametime = 24;
+            delaygrowthtime -= 1;
+            if (delaygrowthtime <= 0)
+            {
+                despawnSize += 0.05f;
+                this.transform.localScale += new Vector3(0f, 0.05f, 0f);
+                this.transform.position += new Vector3(0f, 0.05f, 0f);
+                if (despawnSize >= 3)
+                {
+                    DestoryMe();
+                }
+            }
+        }
+        
+    }
+
+    void DestoryMe()
+    {
+        Debug.Log("Hello from Destroy me");
+        Destroy(gameObject, destoryDelay);
+    }
+}
